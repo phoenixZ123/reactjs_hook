1505af6d4f25434f74362494805d0e93ad92758a
phu nov <phuephue1125@gmail.com>
2023-10-17 12:12:48 +0630

    renderDetail

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+
+import { useEffect, useState } from 'react';
 import './App.css';
 import { Trip } from './components/TripList/Trip';
 function App() {
+  const [data, setData] = useState("hello");
 
+  // if same data render not return back
+  // if cannot same data will be render
+  //use in effect function call to next function will use to useCallBack
+  // array or oject use useRef
+  const change = () => {
+    setData("updated");
 
+  }
+  useEffect(() => {
+    change();
+  }, [change])
   return (
     <div className="App">
+      {data}
       <Trip />
     </div>
   );
